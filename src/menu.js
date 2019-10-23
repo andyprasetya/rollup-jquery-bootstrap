da8ece8ef070ce73de4f4d8147cdf0ac1c35b5d0
Andy Prasetya <andyprasetya@aol.com>
2019-10-23 13:15:26 +0700

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -14,16 +14,10 @@ menu._activateTopNav = function() {
       case 'home':
 
         break;
-      case 'data':
-
-        break;
-      case 'peta':
-
-        break;
       case 'login':
         let loginBoxDOM = templates.loginBox();
         let cmdButtonsDOM = templates.loginButtons();
-        document.getElementById('form_modal_label').innerHTML = "<i class='fa fa-lock'></i>&nbsp;Login SIG SATPOL PP";
+        document.getElementById('form_modal_label').innerHTML = "<i class='fa fa-lock'></i>&nbsp;Login";
         document.getElementById('form_modal_body').innerHTML = loginBoxDOM;
         document.getElementById('form_modal_footer').innerHTML = cmdButtonsDOM;
         $('#modalform').modal({'show':true, 'backdrop':'static'});
@@ -57,15 +51,12 @@ menu._activateTopNav = function() {
               .then(function(data){
                 if (data.response=="201") {
                   document.getElementById('notice').innerHTML = "<div class='alert alert-success pl-0 pr-0 text-center' role='alert'>Login berhasil. Memuat aplikasi...</div>";
-                  sessionStorage.setItem('codex', data.codex);
-                  sessionStorage.setItem('module', data.module);
-                  sessionStorage.setItem('userid', data.userid);
-                  sessionStorage.setItem('username', data.usernm);
-                  sessionStorage.setItem('realname', data.userrn);
+                  /* sessionStorage handling ... */
                   setTimeout(function(){
                     document.getElementById('username').value = '';
                     document.getElementById('password').value = '';
-                    document.location = "../app/";
+                    $('#modalform').modal('hide');
+                    /* reset the page ... */
                   },2000);
                 } else {
                   document.getElementById('notice').innerHTML = "<div class='alert alert-danger pl-0 pr-0 text-center' role='alert'>Login gagal. Ulangi login Anda.</div>";
