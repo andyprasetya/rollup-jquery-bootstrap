fbb039b0b68dc31955543daae9d6731404f8ced0
andyprasetya <andyprasetya@opense7en.com>
2019-10-26 08:56:15 +0700

    set app title to MAPATDA BKAD

--- src/ui-template.js
+++ src/ui-template.js
@@ -2,7 +2,7 @@ var templates = {};
 
 templates.topNavigation = function() {
   let dom = `<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
-    <a class='navbar-brand' href='./'>Application Title</a>
+    <a class='navbar-brand' href='./'>MAPATDA BKAD</a>
     <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
       <span class='navbar-toggler-icon'></span>
     </button>
